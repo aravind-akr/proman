127d022e39e97d983afc0db8757836e47d47d81c
Aravind KR <kraravind1993@gmail.com>
2019-01-30 15:46:41 +0530

    Proman api after password encryption

--- IdeaProjects/proman/proman-api/src/main/java/com/upgrad/proman/api/controller/SignupController.java
+++ IdeaProjects/proman/proman-api/src/main/java/com/upgrad/proman/api/controller/SignupController.java
@@ -2,6 +2,9 @@ package com.upgrad.proman.api.controller;
 
 import com.upgrad.proman.api.model.SignupUserRequest;
 import com.upgrad.proman.api.model.SignupUserResponse;
+import com.upgrad.proman.service.business.SignupBusinessService;
+import com.upgrad.proman.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,13 +12,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
 @RestController
 @RequestMapping("/")
 public class SignupController {
 
-    @RequestMapping(value = RequestMethod.POST,path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @Autowired
+    private SignupBusinessService signUpBusinessService;
+
+
+    @RequestMapping(method = RequestMethod.POST,path = "/signup", consumes = APPLICATION_JSON_UTF8_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest){
-        return new ResponseEntity(HttpStatus.OK);
+
+        final UserEntity userEntity=new UserEntity();
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setMobilePhone(signupUserRequest.getMobileNumber());
+        userEntity.setSalt("1234abc");
+        userEntity.setStatus(4);
+        userEntity.setCreatedAt(ZonedDateTime.now());
+        userEntity.setCreatedBy("api-backend");
+
+        final UserEntity createdUserEntity= signUpBusinessService.signup(userEntity);
+        SignupUserResponse userResponse=new SignupUserResponse().id(createdUserEntity.getUuid()).status("REGISTERED");
+
+        return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
 
     }
 }
